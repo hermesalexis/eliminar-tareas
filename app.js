eace31e568a8a3ed14a93a65b2dadcda519e42c4
hermesalexis <hermesagm@gmail.com>
2017-02-21 15:48:32 -0500

    cambiar tareas por hacer a hechas

---
+++ app.js
@@ -0,0 +1,30 @@
+var elementList = {typeList:'',
+                   htmlItem:'',
+                   item:'', // es el item de la lista
+                   openTagList:'<li>',
+                   closeTagList:'</li>'
+                   };
+
+
+
+var putElementList = function(){
+  $(this.typeList).append(this.openTagList + this.htmlItem + this.closeTagList);
+  this.item.remove();
+}
+
+
+
+$('.todo li').on('click', function(){
+   elementList.htmlItem = $(this).html();
+   elementList.item = $(this);
+   elementList.typeList = '.done';
+   putElementList.call(elementList);// se pasa el contexto
+});
+
+$(".done li').on('click', function(){
+  alert("click");
+  elementList.htmlItem = $(this).html();
+  elementList.item = $(this);
+  elementList.typeList = '.todo';
+  putElementList.call(elementList);// se pasa el contexto
+});
