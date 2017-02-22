5e39575d50dc133df8401163d5f70db3ce0140c2
hermesalexis <hermesagm@gmail.com>
2017-02-21 21:45:39 -0500

    implementa funcionalidad listar tareas y eliminar tareas

--- app.js
+++ app.js
@@ -14,17 +14,38 @@ var putElementList = function(){
 
 
 
-$('.todo li').on('click', function(){
-   elementList.htmlItem = $(this).html();
-   elementList.item = $(this);
-   elementList.typeList = '.done';
-   putElementList.call(elementList);// se pasa el contexto
+
+$(".todo").on("click","li", function(e){
+
+   var target = $(e.target); //permite saber si el elemento span provocó el evento click
+
+   if(target.is("span")){
+     $(this).remove();
+   }else{
+      elementList.htmlItem = $(this).html();
+      elementList.item = $(this);
+      elementList.typeList = '.done';
+      putElementList.call(elementList);// se pasa el contexto
+   }
+
 });
 
-$('.done li').on('click', function(){
-  alert("click");
-  elementList.htmlItem = $(this).html();
-  elementList.item = $(this);
-  elementList.typeList = '.todo';
-  putElementList.call(elementList);// se pasa el contexto
+
+
+$(".done").on("click","li", function(e){ //event delegation debido a que li no se carga inicialmente
+
+  var target = $(e.target);
+
+  if(target.is("span")){
+    $(this).remove();
+  }else{
+    elementList.htmlItem = $(this).html();
+    elementList.item = $(this);
+    elementList.typeList = '.todo';
+    putElementList.call(elementList);// se pasa el contexto
+  }
+
 });
+
+
+
